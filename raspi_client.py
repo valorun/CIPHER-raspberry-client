6c8ed86de17e50b4b59a4f0142ec3ad853635c95
Valorun <ronan@collier-famille.fr>
2018-08-10 18:45:27 +0200

    git ignore

--- raspi_client.py
+++ raspi_client.py
@@ -13,7 +13,7 @@ class ActionType(Enum): # differents types d'actions pris en charge par les rasp
 	RELAY=3
 
 debug=False
-CLIENT_MODES=[ActionType.SERVO]
+CLIENT_MODES=[]
 
 class MotionNamespace(BaseNamespace):
 	def on_command(self, *args):
@@ -106,8 +106,9 @@ def on_disconnect():
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
 
-socketIO = SocketIO('https://localhost', 5000, LoggingNamespace, verify=False)
-#socketIO = SocketIO('http://192.168.1.20', 5000, LoggingNamespace)
+socketIO = SocketIO('http://localhost', 5000, LoggingNamespace, verify=False)
+#socketIO = SocketIO('http://192.168.1.20', 5000, LoggingNamespace, verify=False)
+
 socketIO.on('disconnect', on_disconnect)
 
 raspi_namespace = socketIO.define(RaspiNamespace, '/raspi')
