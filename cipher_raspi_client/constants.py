8c6625af9f437f09da68acab3987e243f1cc7578
valorun <ronan@collier-famille.fr>
2020-02-17 16:01:00 +0100

    new config structure

--- cipher_raspi_client/constants.py
+++ cipher_raspi_client/constants.py
@@ -1,9 +1,3 @@
 from os.path import join, dirname
 
-RASPBERRY_ID='test'
-MQTT_BROKER_URL='localhost'
-MQTT_BROKER_PORT=1883 # default is 1883
-
-LOG_FILE=join(dirname(__file__), 'app.log')
-
-WHEEL_MODE=True
+CONFIG_FILE = join(dirname(__file__), 'config.ini')
