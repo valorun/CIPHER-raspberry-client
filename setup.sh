cb6f6dbddf3e10d3415075765f99baac409dc2cc
valorun <ronan@collier-famille.fr>
2020-12-14 20:59:25 +0100

    setup.sh changes

--- setup.sh
+++ setup.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
 
-install_program(){
-    while true; do
-        read -p "Do you wish to install this program ? " yn
-        case $yn in
-            [Yy]* ) sudo apt-get install "$1"; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-}
-
 ### requirements ###
-if type "python3" &>/dev/null; then
-    echo "Python found"
-else
-    echo "Python3 not found"
-    install_program "python3"
-fi
-
-if type "pip3" &>/dev/null; then
-    echo "Pip found"
-else
-    echo "Pip not found"
-    install_program "python3-pip"
-fi
-
+apt-get -y install "python3"
+apt-get -y install "python3-pip"
 
 APP_PATH=$(cd $(dirname "$0") && pwd)
 
@@ -62,7 +39,7 @@ then
         echo "Program already added on startup."
     else
         while true; do
-            read -p "Do you wish to add this program on startup ? " yn
+            read -p "Do you want to add this program on startup ? " yn
             case $yn in
                 [Yy]* ) sed -i -e "\$i \\nohup sudo $APP_PATH/app.py &\\n" /etc/rc.local; break;;
                 [Nn]* ) exit;;
